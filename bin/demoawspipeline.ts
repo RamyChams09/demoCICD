0404a60a838f3b5c6e52ff8a9a3c02293e7a11e1
Ramy Chamseddin <ramy.chamseddin-el-saghir@capgemini.com>
2023-10-24 10:35:02 +0200

    initial commit

---
+++ bin/demoawspipeline.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { DemoawspipelineStack } from '../lib/demoawspipeline-stack';
+
+const app = new cdk.App();
+new DemoawspipelineStack(app, 'DemoawspipelineStack', {
+  env: { account: '646808865460', region: 'eu-central-1' },
+
+});
